4ebc7d4f6a81fe29cf5d654704b802eb2042fed6
Matthew Jaikaran <mattjaikaran@gmail.com>
2023-12-15 19:13:55 -0500

    fixed loginform

--- components/forms/auth/LoginForm.tsx
+++ components/forms/auth/LoginForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { SubmitHandler, useForm } from 'react-hook-form';
@@ -12,8 +13,6 @@ import { LoginFormInputs } from '@/types/user';
 import Alert from 'react-bootstrap/Alert';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import useAxios from '@/lib/utils/axios';
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // yup validation schema user/password
 const schema = yup
@@ -25,7 +24,6 @@ const schema = yup
 
 const LoginForm = () => {
   const router = useRouter();
-  const api = useAxios();
   const dispatch = useAppDispatch();
   const [error, setError] = useState('');
   const USER_LOCAL_STORAGE_KEY = 'USER_KEY';
@@ -45,9 +43,7 @@ const LoginForm = () => {
         username: data.username,
         password: data.password,
       };
-      // eslint-disable-next-line react-hooks/rules-of-hooks
       const response = await useLogin(userInfo);
-      const user = response.data.user;
 
       if (response.status === 403) {
         setError(response.data.errors.detail);
@@ -57,20 +53,44 @@ const LoginForm = () => {
       }
       if (response.status === 200) {
         try {
-          const userResponse = await api.get(
-            `${API_URL}/users/${response.data?.user.id}/`
-          );
-          const apiUser = userResponse.data;
-          apiUser.token = response.data.access;
-          apiUser.refresh = response.data.refresh;
-          localStorage.setItem(USER_LOCAL_STORAGE_KEY, JSON.stringify(apiUser));
-          localStorage.setItem('AUTHTOKEN', JSON.stringify(user.access));
+          let user = {
+            about: '',
+            all_time_revenue: '',
+            company: '',
+            date_joined: '',
+            email: '',
+            first_name: '',
+            id: '',
+            img_url: '',
+            is_active: false,
+            is_sales_rep: true,
+            is_staff: false,
+            is_superuser: false,
+            last_login: '',
+            last_name: '',
+            leaderboard_access: false,
+            linkedin_profile: '',
+            market_type: '',
+            profile_visibility: '',
+            references: [],
+            title: '',
+            user_fit_score: 0,
+            user_status: '',
+            username: '',
+            token: '',
+            refresh: '',
+          };
+          user = response.data.user;
+          user.token = response.data.access;
+          user.refresh = response.data.refresh;
+          localStorage.setItem(USER_LOCAL_STORAGE_KEY, JSON.stringify(user));
+          localStorage.setItem('AUTHTOKEN', JSON.stringify(user.token));
           localStorage.setItem('REFRESH_TOKEN', JSON.stringify(user.refresh));
           // set user data in global state
-          dispatch(userLogin(apiUser));
+          dispatch(userLogin(user));
           // if user.first_name is null, redirect to /profile/edit to complete profile
           // otherwise, redirect to /profile
-          if (!apiUser.first_name) {
+          if (!user.first_name && !user.last_name) {
             router.push('/profile/edit');
           } else {
             router.push('/profile');
@@ -94,7 +114,6 @@ const LoginForm = () => {
           <Form.Label>Username</Form.Label>
           <Form.Control
             type="text"
-            // default value router.query.username
             defaultValue={router.query.username}
             placeholder="Enter username"
             {...register('username')}
